fa7705c5bc63b685c4d27e803d9b3483625a0d83
YuriSPS <yuristephanops@gmail.com>
2024-06-13 09:59:29 -0300

    adicionado funcao em js que ativa items na pagina orcamento pegando valores que foram passados na url apos clique

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
+// Ativar links do menu
 const linksMenu = document.querySelectorAll(".header-menu li a");
-console.log(linksMenu);
 
 function menuAtivo(item) {
   const urlAtual = window.location.href;
@@ -9,3 +9,12 @@ function menuAtivo(item) {
 }
 
 linksMenu.forEach(menuAtivo);
+
+// Ativar items do orçamento através dos parâmetros passados na URL
+const paramsUrl = new URLSearchParams(window.location.search);
+paramsUrl.forEach((params) => {
+  const formOrcamento = document.getElementById(params);
+  if (formOrcamento) {
+    formOrcamento.checked = true;
+  }
+});
